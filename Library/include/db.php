c601a9befb2c66a494bd1ed63c40c6a6a1e28531
lion2486 <lion2486@2ca3b0e5-7a80-4dc1-ac40-edeb18cfc453>
2011-10-30 15:28:56 +0000

    db some changes

--- Library/include/db.php
+++ Library/include/db.php
@@ -3,4 +3,6 @@
 	$db_hostname = "127.0.0.1";
 	$db_username = "root";
 	$db_password = "some_pass";
+	
+	$db_name = "kostis";
 ?>
--- Library/model/db.php
+++ Library/model/db.php
@@ -1,7 +1,7 @@
 ﻿<?php
 function connect(){
 	$con = mysql_connect($db_hostname, $db_username, $db_password) or die(mysql_error());
-	mysql_select_db('kostis', $con) or die(mysql_error());
+	mysql_select_db($db_name, $con) or die(mysql_error());
 	mysql_query("SET NAMES 'utf8'", $con) or die(mysql_error());
 	return $con;
 }
