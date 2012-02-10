1775717efef186782109beffad2801c9e1aead70
Sudavar <Sudavar@2ca3b0e5-7a80-4dc1-ac40-edeb18cfc453>
2012-02-10 01:19:40 +0000

    Styling Search is ready

--- Library/view/search.php
+++ Library/view/search.php
@@ -2,83 +2,85 @@
 	if(!defined('VIEW_NAV'))
 		die("Invalid request!");
 	define('VIEW_SHOW', true);
-if(!isset($_GET['title'])){?>
+if(!isset($_GET['search'])){?>
     <div id="direction"><a href="index.php">Αρχική</a>&nbsp;&gt;&gt;&nbsp;Αναζήτηση</div>
 <?php
 }else{ ?>
-    <div id="direction"><a href="index.php">Αρχική</a>&nbsp;&gt;&gt;&nbsp;<a href="index.php?show=search">Αναζήτηση</a>&nbsp;&gt;&gt;&nbsp;<?php if(isset($_GET['title'])) echo $_GET['search']; ?></div>
+    <div id="direction"><a href="index.php">Αρχική</a>&nbsp;&gt;&gt;&nbsp;<a href="index.php?show=search">Αναζήτηση</a>&nbsp;&gt;&gt;&nbsp;<?php echo $_GET['search']; ?></div>
 <?php } ?>
 <div class="content">
 	<form action="index.php" method="get" id="search">
-		<div id="search-head">Αναζήτηση</div>
 		<div id="search_main">
-			<div class="subtitle">Εισάγετε λέξη-κλειδί για αναζήτηση</div>
-			<input type="text" name="search" onclick="this.value='';"<?php /*this.select()" */ ?> onfocus="this.select()" 
+			<div class="search_head block">Αναζητήστε τον τίτλο-λέξη που σας ενδιαφέρει:</div>
+			<div class="block">
+				<input id="search_word" type="text" name="search" onclick="this.value='';"<?php /*this.select()" */ ?> onfocus="this.select()" 
 					onblur="this.value=!this.value?'<?php echo (isset($_GET['search'])) ? $_GET['search'] : "Εισάγεται κείμενο εδώ..."; ?>':this.value;" 
 					value="<?php echo (isset($_GET['search'])) ? $_GET['search'] : "Εισάγεται κείμενο εδώ..."; ?>" />
-			
+				<input id="search_do" type="submit" value="Αναζήτηση" />
+				<div id="search_subtitle">Εισάγετε λέξη-κλειδί για αναζήτηση</div>
+        	</div>
 		</div>
 		<input type="hidden" name="show" value="search" />
 		<input type="hidden" name="do" value="search" />
-		<div id="search-in">Αναζήτηση σε 
-			<div class="search_rad" id="book-div">
-				<input type="radio" name="search-type" id="books" value="books" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "books") ? "checked=\"checked\"" : ((isset($_GET['do'])) ? "" : "checked=\"checked\""); ?>/><label for="books">Βιβλία</label>
-			</div>
-			<div class="search_rad">
-				<input type="radio" name="search-type" id="newspapers" value="newspapers" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "newspapers") ? "checked=\"checked\"" : ""; ?>/><label for="newspapers">Εφημερίδες</label>
-			</div>
-			<div class="search_rad">
-				<input type="radio" name="search-type" id="manuscripts" value="manuscripts" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "manuscripts") ? "checked=\"checked\"" : ""; ?>/><label for="manuscripts">Χειρόγραφα</label>
-			</div>
-			<div class="search_rad">
-				<input type="radio" name="search-type" id="articles" value="articles" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "articles") ? "checked=\"checked\"" : ""; ?>/><label for="articles">Άρθρα</label>
-			</div>
+		<div id="search_extra">
+    		<div class="search_head block search_cat">Αναζητήση ανάμεσα σε</div>
+    		<div id="search-in" class="block">
+    			<div class="search_rad block bold" id="book-div">
+    				<input type="radio" name="search-type" id="books" value="books" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "books") ? "checked=\"checked\"" : ((isset($_GET['do'])) ? "" : "checked=\"checked\""); ?>/><label for="books">Βιβλία</label>
+    			</div>
+    			<div class="search_rad block bold">
+    				<input type="radio" name="search-type" id="newspapers" value="newspapers" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "newspapers") ? "checked=\"checked\"" : ""; ?>/><label for="newspapers">Εφημερίδες</label>
+    			</div>
+    			<div class="search_rad block bold">
+    				<input type="radio" name="search-type" id="manuscripts" value="manuscripts" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "manuscripts") ? "checked=\"checked\"" : ""; ?>/><label for="manuscripts">Χειρόγραφα</label>
+    			</div>
+    			<div class="search_rad block bold">
+    				<input type="radio" name="search-type" id="articles" value="articles" <?php echo (isset($_GET['search-type']) && $_GET['search-type'] == "articles") ? "checked=\"checked\"" : ""; ?>/><label for="articles">Άρθρα</label>
+    			</div>
+    		</div>
+    		<div id="search-book" class="search_category">
+    			<div class="search_chk block bold">
+    				<label for="writer">Συγγραφέας</label><input type="text" name="writer" id="writer" <?php echo (isset($_GET['writer'])) ? "value=\"".$_GET['writer']."\"" : ""; ?>/>
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="publisher">Εκδότης</label><input type="text" name="publisher" id="publisher" <?php echo (isset($_GET['publisher'])) ? "value=\"".$_GET['publisher']."\"" : ""; ?>/>
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="isbn">ISBN</label><input type="text" name="isbn" id="isbn" <?php echo (isset($_GET['isbn'])) ? "checked=\"".$_GET['isbn']."\"" : ""; ?>/>
+    			</div>
+    			<div class="search_chk block bold" style="width: 170px; margin: 0px;">
+    				<input style="float: left;" type="checkbox" value="1" name="available" id="available" <?php echo (isset($_GET['available'])) ? "checked=\"checked\"" : ""; ?> />
+    				<label for="available">Διαθέσιμο για δανεισμό</label>
+    			</div>
+    		</div>
+    		<div id="search-newspapers" class="search_category">
+    			<div class="search_chk block bold">
+    				<label for="">Εκδόσεις</label><input type="text" name="" id="" />
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="">Έτος</label><input type="text" name="" id="" />
+    			</div>
+    		</div>
+    		<div id="search-manuscripts" class="search_category">
+    			<div class="search_chk block bold">
+    				<label for="">Συγγραφέας</label><input type="text" name="" id="" />
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="">Έτος</label><input type="text" name="" id="" />
+    			</div>
+    		</div>
+    		<div id="search-articles" class="search_category">
+    			<div class="search_chk block bold">
+    				<label for="">Συγγραφέας</label><input type="text" name="" id="" />
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="">Έτος</label><input type="text" name="" id="" />
+    			</div>
+    			<div class="search_chk block bold">
+    				<label for="">Εμφανίσεις</label><input type="text" name="" id="" />
+    			</div>
+    		</div>
 		</div>
-		
-		<div id="search-book" style="display: none;">
-		
-			<div class="search_chk">
-				<label for="writer">Συγγραφέας</label><input type="text" name="writer" id="writer" <?php echo (isset($_GET['writer'])) ? "value=\"".$_GET['writer']."\"" : ""; ?>/>
-			</div>
-			<div class="search_chk">
-				<label for="publisher">Εκδότης</label><input type="text" name="publisher" id="publisher" <?php echo (isset($_GET['publisher'])) ? "value=\"".$_GET['publisher']."\"" : ""; ?>/>
-			</div>
-			<div class="search_chk">
-				<label for="isbn">ISBN</label><input type="text" name="isbn" id="isbn" <?php echo (isset($_GET['isbn'])) ? "checked=\"".$_GET['isbn']."\"" : ""; ?>/>
-			</div>
-			<div class="search_chk">
-				<label for="">Διαθέσιμο για δανεισμό</label><input type="checkbox" value="1" name="available" id="available" <?php echo (isset($_GET['available'])) ? "checked=\"checked\"" : ""; ?> />
-			</div>
-		</div>
-		<div id="search-newspapers" style="display: none;">
-			<div class="search_chk">
-				<label for="">Εκδόσεις</label><input type="text" name="" id="" />
-			</div>
-			<div class="search_chk">
-				<label for="">Έτος</label><input type="text" name="" id="" />
-			</div>
-		</div>
-		<div id="search-manuscripts" style="display: none;">
-			<div class="search_chk">
-				<label for="">Συγγραφέας</label><input type="text" name="" id="" />
-			</div>
-			<div class="search_chk">
-				<label for="">Έτος</label><input type="text" name="" id="" />
-			</div>
-		</div>
-		<div id="search-articles" style="display: none;">
-			<div class="search_chk">
-				<label for="">Συγγραφέας</label><input type="text" name="" id="" />
-			</div>
-			<div class="search_chk">
-				<label for="">Έτος</label><input type="text" name="" id="" />
-			</div>
-			<div class="search_chk">
-				<label for="">Εμφανίσεις</label><input type="text" name="" id="" />
-			</div>
-		</div>
-		<input type="submit" value="Αναζήτηση" />
-			<br />
 	</form>
 	<script type="text/javascript">
 		
