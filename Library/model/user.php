890e6169bf832d573b24e302248c7e6afd195653
lion2486 <lion2486@2ca3b0e5-7a80-4dc1-ac40-edeb18cfc453>
2012-07-27 18:05:26 +0000

    some work on user-profile picture chhange

--- Library/model/user.php
+++ Library/model/user.php
@@ -175,20 +175,20 @@ class User{
 		?>
         <div class="block" id="user-left">
 			<img src="view/images/user-icon.png" alt="User Images" /><br />
-			<form action="" id="uploadForm" >
-				<input type="file" name="profilePicture" />
+			<form action="" id="uploadForm" method="post" enctype="multipart/form-data" >
+				<input type="file" name="profilePicture" id="profilePicture" />
 				<input type="hidden" name="hidden" value="1" />
 			</form>
 		<?php 
 		//TODO use the script localy 
 		?>
 			<script src="http://malsup.github.com/jquery.form.js"></script>
-			<script> 
+			<?php /*<script> 
 		        // wait for the DOM to be loaded 
 		        $(document).ready(function() { 
-		        	$('#uploadForm profilePicture').change(function() { $('#uploadForm').submit(); });
+		        //	$('#uploadForm #profilePicture').change(function() { document.forms["uploadForm"].submit();});
 
-		        			
+		        $('#uploadForm #profilePicture').change(function() { document.forms["uploadForm"].ajaxSubmit();});
 		            // bind 'myForm' and provide a simple callback function 
 		            $('#uploadForm').ajaxForm(function() { 
 		                alert("Thank you for your comment!"); 
@@ -196,6 +196,78 @@ class User{
 		            }); 
 		        }); 
     		</script> 
+    		*/ 
+		//TODO add ajax submit to form
+		// maybe from http://jquery.malsup.com/form
+		?>
+    		<script type="text/javascript">
+	    		$(document).ready(function() { 
+
+	    			$('#uploadForm #profilePicture').change(function() { document.forms["uploadForm"].submit();});
+	    			
+	    		    var options = { 
+	    		        target:        '#output2',   // target element(s) to be updated with server response 
+	    		        beforeSubmit:  showRequest,  // pre-submit callback 
+	    		        success:       showResponse  // post-submit callback 
+	    		        
+	    		        // other available options: 
+	    		        //url:       url         // override for form's 'action' attribute 
+	    		        //type:      type        // 'get' or 'post', override for form's 'method' attribute 
+	    		        //dataType:  null        // 'xml', 'script', or 'json' (expected server response type) 
+	    		        //clearForm: true        // clear all form fields after successful submit 
+	    		        //resetForm: true        // reset the form after successful submit 
+	    		 
+	    		        // $.ajax options can be used here too, for example: 
+	    		        //timeout:   3000 
+	    		    }; 
+	    		 
+	    		    // bind to the form's submit event 
+	    		    $('#uploadForm').submit(function() { 
+	    		        // inside event callbacks 'this' is the DOM element so we first 
+	    		        // wrap it in a jQuery object and then invoke ajaxSubmit 
+	    		        $(this).ajaxSubmit(options); 
+	    		 
+	    		        // !!! Important !!! 
+	    		        // always return false to prevent standard browser submit and page navigation 
+	    		        return false; 
+	    		    }); 
+	    		}); 
+	    		 
+	    		// pre-submit callback 
+	    		function showRequest(formData, jqForm, options) { 
+	    		    // formData is an array; here we use $.param to convert it to a string to display it 
+	    		    // but the form plugin does this for you automatically when it submits the data 
+	    		    var queryString = $.param(formData); 
+	    		 
+	    		    // jqForm is a jQuery object encapsulating the form element.  To access the 
+	    		    // DOM element for the form do this: 
+	    		    // var formElement = jqForm[0]; 
+	    		 
+	    		    alert('About to submit: \n\n' + queryString); 
+	    		 
+	    		    // here we could return false to prevent the form from being submitted; 
+	    		    // returning anything other than false will allow the form submit to continue 
+	    		    return true; 
+	    		} 
+	    		 
+	    		// post-submit callback 
+	    		function showResponse(responseText, statusText, xhr, $form)  { 
+	    		    // for normal html responses, the first argument to the success callback 
+	    		    // is the XMLHttpRequest object's responseText property 
+	    		 
+	    		    // if the ajaxSubmit method was passed an Options Object with the dataType 
+	    		    // property set to 'xml' then the first argument to the success callback 
+	    		    // is the XMLHttpRequest object's responseXML property 
+	    		 
+	    		    // if the ajaxSubmit method was passed an Options Object with the dataType 
+	    		    // property set to 'json' then the first argument to the success callback 
+	    		    // is the json data object returned by the server 
+	    		 
+	    		    alert('status: ' + statusText + '\n\nresponseText: \n' + responseText + 
+	    		        '\n\nThe output div should have already been updated with the responseText.'); 
+	    		} 
+
+    		</script>
 			<!-- <a href="#">Αλλάξτε την φωτογραφία</a> --><br />
 			<br /><span class="bold">Όνομα Χρήστη:</span> <?php echo $row['username']; ?>
 			<br /><span class="bold">Τύπος Χρήστη:</span> <?php echo $row['usertype']; ?>
