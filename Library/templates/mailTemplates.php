12474f08c2abbaf54ee050664936f1660f075bba
lion2486 <lion2486@2ca3b0e5-7a80-4dc1-ac40-edeb18cfc453>
2012-10-16 09:39:23 +0000

    fixes on previus commit

--- Library/templates/mailTemplates.php
+++ Library/templates/mailTemplates.php
@@ -1,47 +1,46 @@
 <?php	
 	class customMail{
-		var $mailTemplate = array();
 		var $m;
 	
 		public function  __contruct($type){
 			global $CONFIG;
 			
-			$m = new MAIL;
+			$this->m = new MAIL;
 			
 			$mailTemplate['registerUser']['title'] = "Εγγραφή νέου μέλους - {$CONFIG['title']}";
 			$mailTemplate['registerUser']['body'] = "
 				Ευχατιστούμε πολύ για την εγγραφή σας στην πύλη της δανειστικής μας βιβλιοθήκης. 
 				Θα χαρούμε να τη χρεισημοποιήσετε και να σας φανεί χρήσιμη\n{$CONFIG['url']}\n\nΗ Ομάδα Διαχείρησης";
 			
-			$m->Subject($mailTemplate[$type]['title']);
-			$m->Text($mailTemplate[$type]['body']);
+			$this->m->Subject($mailTemplate[$type]['title']);
+			$this->m->Text($mailTemplate[$type]['body']);
 		}
 		
 		public function sentMail(){
 			global $CONFIG;
 			
 			//$m = new MAIL;
-			$m->From($CONFIG['admin_email']);
+			$this->m->From($CONFIG['admin_email']);
 			//$m->AddTo($to);
 			//$m->Subject($subject);
 			//$m->Text($text);
 			
 			if($CONFIG['MAIL']['USE_SMTP'])
-				$c = $m->Connect($CONFIG['MAIL']['SMTP']['HOSTNAME'], $CONFIG['MAIL']['SMTP']['PORT'], $CONFIG['MAIL']['SMTP']['USERNAME'], $CONFIG['MAIL']['SMTP']['PASSWORD']) or die(print_r($m->Result));
+				$c = $this->m->Connect($CONFIG['MAIL']['SMTP']['HOSTNAME'], $CONFIG['MAIL']['SMTP']['PORT'], $CONFIG['MAIL']['SMTP']['USERNAME'], $CONFIG['MAIL']['SMTP']['PASSWORD']) or die(print_r($m->Result));
 			
 			if($CONFIG['MAIL']['USE_SMTP'])
-				$res = $m->Send($c);
+				$res = $this->m->Send($c);
 			else
-				$res = $m->Send();
+				$res = $this->m->Send();
 				
 			echo $res ? '<p class=\"succes\">Mail sent !</p>' : '<p class=\"error\">Error !</p>';
 			
 			if($CONFIG['MAIL']['USE_SMTP'])
-				$m->Disconnect();
+				$this->m->Disconnect();
 		}
 		
 		public function AddTo($TO){
-			$m->AddTo($TO);
+			$this->m->AddTo($TO);
 		}
 		
 	};
